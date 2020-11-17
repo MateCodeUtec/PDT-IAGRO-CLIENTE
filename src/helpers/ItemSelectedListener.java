3b883395d331243f013bbe79c52487bf7eefca38
lucaspiegas <lucas.piegas@estudiantes.utec.com.uy>
2020-11-17 18:42:12 -0300

    Alta de formulario y de casillas, hay que dibujar el formulario ahora

---
+++ src/helpers/ItemSelectedListener.java
@@ -0,0 +1,11 @@
+package helpers;
+
+import java.util.List;
+
+import models.Parametro;
+
+public interface ItemSelectedListener {
+	
+	List<Parametro> onItemSelected(List<Parametro> myFormItems);
+
+}
--- src/views/form/param/FrmParamAM.java
+++ src/views/form/param/FrmParamAM.java
@@ -1,4 +1,4 @@
-package views.form;
+package views.form.param;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -9,6 +9,7 @@ import javax.swing.border.EmptyBorder;
 import java.awt.Color;
 import java.awt.Toolkit;
 import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
 
 import enums.AccionFormulario;
 import enums.TipoDato;
@@ -27,13 +28,13 @@ import services.TipoParametroBeanRemote;
 import services.UsuarioBean;
 import services.UsuarioBeanRemote;
 import views.auth.FrmPrincipal;
-import views.form.param.FrmParamAM;
-import views.usuario.FrmUsuarioAM;
+import views.form.FrmFormAM;
 
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
+import java.awt.SystemColor;
 import java.awt.TextField;
 
 import javax.naming.InitialContext;
@@ -49,6 +50,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JSeparator;
+import javax.swing.JTable;
 import javax.swing.UIManager;
 import javax.swing.JPasswordField;
 import javax.swing.JComboBox;
@@ -56,28 +58,29 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import javax.swing.JTextArea;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 
-public  class FrmFormAM extends JFrame {
+public class FrmParamAM extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField txtTitulo;
+	private JTextField txtNombre;
 	private FormularioBeanRemote formularioBean = null;
 	private TipoParametroBeanRemote tipoParametroBean = null;
 	private ParametroBeanRemote parametroBean = null;
-	public static List<Parametro> listaParametros;
-	private FrmParamAM frm;
+	private JTextField txtTipoDato;
+	private JScrollPane scrollPaneParam;
+	private JTable jtable_param;
 
 	/**
 	 * Create the frame. 
 	 */
-	public FrmFormAM(AccionFormulario accion, Usuario u) {
+	public FrmParamAM() {
 		
-		listaParametros = new ArrayList<Parametro>();
 		
 		setUndecorated(true);
 		setTitle("IAGRO - Principal");
 		setIconImage(Toolkit.getDefaultToolkit()
-				.getImage(FrmFormAM.class.getResource("/views/assets/icons/icon-app-barra.png")));
+				.getImage(FrmParamAM.class.getResource("/views/assets/icons/icon-app-barra.png")));
 		setBounds(100, 100, 491, 393);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
@@ -99,14 +102,14 @@ public  class FrmFormAM extends JFrame {
 		panel.setBackground(new Color(119, 184, 105));
 		panel.setLayout(null);
 
-		JLabel lblTitulo = new JLabel("Alta de formulario");
+		JLabel lblTitulo = new JLabel("Alta de parametro");
 		lblTitulo.setFont(new Font("Segoe UI", Font.BOLD, 12));
 		lblTitulo.setBounds(39, 11, 257, 14);
 		panel.add(lblTitulo);
 
 		JLabel lblNewLabel_2 = new JLabel("");
 		lblNewLabel_2
-				.setIcon(new ImageIcon(FrmFormAM.class.getResource("/views/assets/icons/icon-app-ventana.png")));
+				.setIcon(new ImageIcon(FrmParamAM.class.getResource("/views/assets/icons/icon-app-ventana.png")));
 		lblNewLabel_2.setBounds(10, 11, 19, 16);
 		panel.add(lblNewLabel_2);
 
@@ -137,25 +140,25 @@ public  class FrmFormAM extends JFrame {
 		
 		
 
-		txtTitulo = new JTextField();
-		txtTitulo.setVerifyInputWhenFocusTarget(false);
-		txtTitulo.setForeground(new Color(0, 0, 0));
-		txtTitulo.setFont(new Font("Arial", Font.PLAIN, 12));
-		txtTitulo.setColumns(10);
-		txtTitulo.setBorder(null);
-		txtTitulo.setBackground(new Color(255, 255, 255));
-		txtTitulo.setBounds(31, 74, 199, 33);
-		panel_2.add(txtTitulo);
+		txtNombre = new JTextField();
+		txtNombre.setVerifyInputWhenFocusTarget(false);
+		txtNombre.setForeground(new Color(0, 0, 0));
+		txtNombre.setFont(new Font("Arial", Font.PLAIN, 12));
+		txtNombre.setColumns(10);
+		txtNombre.setBorder(null);
+		txtNombre.setBackground(new Color(255, 255, 255));
+		txtNombre.setBounds(31, 91, 199, 33);
+		panel_2.add(txtNombre);
 
 		JSeparator separator = new JSeparator();
 		separator.setForeground(Color.LIGHT_GRAY);
 		separator.setBackground(new Color(248, 248, 255));
-		separator.setBounds(31, 111, 199, 10);
+		separator.setBounds(31, 128, 199, 10);
 		panel_2.add(separator);
 
-		JLabel lblNewLabel_1_1 = new JLabel("Titulo");
+		JLabel lblNewLabel_1_1 = new JLabel("Nombre");
 		lblNewLabel_1_1.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		lblNewLabel_1_1.setBounds(31, 49, 199, 14);
+		lblNewLabel_1_1.setBounds(31, 66, 199, 14);
 		panel_2.add(lblNewLabel_1_1);
 
 		JButton btnCancelar = new JButton("Cancelar");
@@ -168,12 +171,12 @@ public  class FrmFormAM extends JFrame {
 		btnCancelar.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		btnCancelar.setBorder(new LineBorder(new Color(240, 248, 255)));
 		btnCancelar.setBackground(new Color(102, 204, 0));
-		btnCancelar.setBounds(143, 352, 102, 33);
+		btnCancelar.setBounds(360, 352, 102, 33);
 		panel_2.add(btnCancelar);
 
-		JLabel lblNewLabel_1_1_2 = new JLabel("Descripcion");
+		JLabel lblNewLabel_1_1_2 = new JLabel("Tipo de dato");
 		lblNewLabel_1_1_2.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-		lblNewLabel_1_1_2.setBounds(31, 131, 199, 14);
+		lblNewLabel_1_1_2.setBounds(31, 130, 199, 14);
 		panel_2.add(lblNewLabel_1_1_2);
 
 		JSeparator separator_2_1 = new JSeparator();
@@ -181,74 +184,15 @@ public  class FrmFormAM extends JFrame {
 		separator_2_1.setBackground(new Color(248, 248, 255));
 		separator_2_1.setBounds(31, 340, 431, 10);
 		panel_2.add(separator_2_1);
-		
-		
-		JTextArea taDescripcion = new JTextArea();
-		taDescripcion.setBorder(new LineBorder(new Color(102, 204, 51)));
-		taDescripcion.setBounds(31, 155, 199, 65);
-		panel_2.add(taDescripcion);
 
 		JButton btnGuardar = new JButton("Guardar");
 		btnGuardar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				var titulo = txtTitulo.getText();
-				var descripcion = taDescripcion.getText();
+				setVisible(false);
+				FrmFormAM.listarParametros();
+		
 				
-				/*
-				if (Validaciones.esVacio(nombre) || Validaciones.esVacio(apellido) || Validaciones.esVacio(usuario)
-						|| Validaciones.esVacio(pass) || Validaciones.esVacio(r.getNombre())) {
-					JOptionPane.showMessageDialog(null, "Todos los campos son obligatorios", "Ups!",
-							JOptionPane.ERROR_MESSAGE);
-				} else {
-				*/
-				/*
-					//Armo el objeto formulario
-					var listaParametros = new ArrayList<Parametro>();
-					
-					TipoParametro tp = new TipoParametro("Cantidad de agua", TipoDato.STRING);
-					try {
-						tp = tipoParametroBean.crear(tp);
-						JOptionPane.showMessageDialog(null, tp.toString());
-						
-					} catch (Exception e1) {
-						JOptionPane.showMessageDialog(null, e1.getMessage(), "Ups!",
-								JOptionPane.ERROR_MESSAGE);
-						e1.printStackTrace();
-					}
-					
-					Parametro p = new Parametro(true, tp);
-					try {
-						p = parametroBean.crear(p);
-						JOptionPane.showMessageDialog(null, tp.toString());
-						
-					} catch (Exception e1) {
-						JOptionPane.showMessageDialog(null, e1.getMessage(), "Ups!",
-								JOptionPane.ERROR_MESSAGE);
-						e1.printStackTrace();
-					}
-					listaParametros.add(p);
-					*/
-					Formulario f = new Formulario();
-					f.setTitulo(titulo);
-					f.setDescripcion(descripcion);
-					f.setVisibilidad(Visibilidad.PUBLICO);
-					f.setUsuario(u);
-					f.setParametros(listaParametros);
-					
-					try {
-						formularioBean.crear(f);
-						JOptionPane.showMessageDialog(null, "Formulario guardado con exito");
-												
-						setVisible(false);
-						
-					} catch (Exception e1) {
-						JOptionPane.showMessageDialog(null, e1.getMessage(), "Ups!",
-								JOptionPane.ERROR_MESSAGE);
-						e1.printStackTrace();
-					}
-		//		}
-
 			}
 
 		});
@@ -263,39 +207,72 @@ public  class FrmFormAM extends JFrame {
 		lblId.setVisible(false);
 		lblId.setBounds(388, 317, 45, 13);
 		panel_2.add(lblId);
-
-		JButton btnModificar = new JButton("Modificar");
-		btnModificar.setForeground(Color.WHITE);
-		btnModificar.setFont(new Font("Segoe UI", Font.PLAIN, 14));
-		btnModificar.setBorder(new LineBorder(new Color(240, 248, 255)));
-		btnModificar.setBackground(new Color(119, 184, 105));
-		btnModificar.setBounds(31, 352, 102, 33);
-		panel_2.add(btnModificar);
 		
-		
-		JSeparator separator_1 = new JSeparator();
-		separator_1.setForeground(Color.LIGHT_GRAY);
-		separator_1.setBackground(new Color(248, 248, 255));
-		separator_1.setBounds(31, 230, 431, 10);
-		panel_2.add(separator_1);
-		
-		JButton btnAgregarParam = new JButton("Nueva casilla");
+		JButton btnAgregarParam = new JButton("Agregar Parametro");
 		btnAgregarParam.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				FrmParamAM frm = new FrmParamAM();
-				frm.setVisible(true);
+				TipoParametro tp = new TipoParametro(txtNombre.getText(), TipoDato.STRING);
 				
+				try {
+					tp = tipoParametroBean.crear(tp);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				Parametro p = new Parametro(true,tp);
+				
+				try {
+					p = parametroBean.crear(p);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+				FrmFormAM.listaParametros.add(p);
+				construirTabla();
+					
 			}
 		});
 		btnAgregarParam.setForeground(Color.WHITE);
 		btnAgregarParam.setFont(new Font("Segoe UI", Font.PLAIN, 14));
 		btnAgregarParam.setBorder(new LineBorder(new Color(240, 248, 255)));
 		btnAgregarParam.setBackground(new Color(102, 204, 0));
-		btnAgregarParam.setBounds(31, 296, 144, 33);
+		btnAgregarParam.setBounds(31, 244, 144, 33);
 		panel_2.add(btnAgregarParam);
 		
-
+		JSeparator separator_2 = new JSeparator();
+		separator_2.setForeground(Color.LIGHT_GRAY);
+		separator_2.setBackground(new Color(248, 248, 255));
+		separator_2.setBounds(31, 192, 199, 10);
+		panel_2.add(separator_2);
+		
+		txtTipoDato = new JTextField();
+		txtTipoDato.setVerifyInputWhenFocusTarget(false);
+		txtTipoDato.setForeground(Color.BLACK);
+		txtTipoDato.setFont(new Font("Arial", Font.PLAIN, 12));
+		txtTipoDato.setColumns(10);
+		txtTipoDato.setBorder(null);
+		txtTipoDato.setBackground(Color.WHITE);
+		txtTipoDato.setBounds(31, 155, 199, 33);
+		panel_2.add(txtTipoDato);
+		
+		scrollPaneParam = new JScrollPane();
+		scrollPaneParam.setBounds(251, 49, 211, 258);
+		panel_2.add(scrollPaneParam);
+		
+		jtable_param = new JTable();
+		jtable_param.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"Nombre", "Tipo de Dato"
+			}
+		));
+		scrollPaneParam.setViewportView(jtable_param);
+		construirTabla();
+		
+		
+		/*
 		// Configurar ventana
 		btnGuardar.setVisible(false);
 		btnModificar.setVisible(false);
@@ -310,8 +287,44 @@ public  class FrmFormAM extends JFrame {
 			// Cargo datos
 			
 		}
+		*/
 
 	}
+	
+	/**
+	 * Contruimos la tabla en el ScrollPane
+	 */
+	public void construirTabla() {
+		
+		String titulos [] = { "Nombre", "Tipo de Parametro"};
+		
+		String informacion [][] = obtenerMatriz();
+		
+		jtable_param = new JTable(informacion, titulos);
+		jtable_param.setBackground(SystemColor.activeCaptionBorder);
+		scrollPaneParam.setViewportView(jtable_param);
+	}
+	
+	/**
+	 * Completamos la tabla con datos
+	 * @return matriz [][]
+	 */
+	private String[][] obtenerMatriz() {
+		ArrayList<Parametro> miLista = null;	
+		miLista = (ArrayList<Parametro>) FrmFormAM.listaParametros;
+		
+		String matrizInfo[][] = new String [miLista.size()][2];
+		
+		for( int i = 0 ; i< miLista.size() ; i++) {
+			
+			matrizInfo[i][0] = miLista.get(i).getTipo().getNombre() + "";
+			matrizInfo[i][1] = miLista.get(i).getTipo().getTipo() + "";
+			
+		}
+		return matrizInfo;
+	}
+
+
 
 	public void limpiarTextFields(ArrayList<JTextField> campos) {
 
@@ -320,13 +333,4 @@ public  class FrmFormAM extends JFrame {
 		}
 
 	}
-	
-	public static void listarParametros() {
-		
-		for (Parametro p : listaParametros) {
-			System.out.println(p.getTipo().getNombre());
-		}
-		
-	}
-	
 }
